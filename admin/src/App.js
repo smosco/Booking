2cfac09f028e39c5aa5b2cc6092e7c40155884f5
ore <go1ruf2tk3@gmail.com>
2023-08-18 13:30:09 +0900

    fix: 쿠키 접근할때 CORS 에러 해결

---
+++ admin/src/App.js
@@ -0,0 +1,100 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
+  const ProtectRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+
+    if (!user) {
+      return <Navigate to="login" />;
+    }
+
+    return children;
+  };
+
+  return (
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="login" element={<Login />} />
+            <Route
+              index
+              element={
+                <ProtectRoute>
+                  <Home />
+                </ProtectRoute>
+              }
+            />
+            <Route path="users">
+              <Route
+                index
+                element={
+                  <ProtectRoute>
+                    <List />
+                  </ProtectRoute>
+                }
+              />
+              <Route
+                path=":userId"
+                element={
+                  <ProtectRoute>
+                    <Single />
+                  </ProtectRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectRoute>
+                    <New inputs={userInputs} title="Add New User" />
+                  </ProtectRoute>
+                }
+              />
+            </Route>
+            <Route path="products">
+              <Route
+                index
+                element={
+                  <ProtectRoute>
+                    <List />
+                  </ProtectRoute>
+                }
+              />
+              <Route
+                path=":productId"
+                element={
+                  <ProtectRoute>
+                    <Single />
+                  </ProtectRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectRoute>
+                    <New inputs={productInputs} title="Add New Product" />
+                  </ProtectRoute>
+                }
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
